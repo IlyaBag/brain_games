46b80fd227cc85b7d6ea1d75186bd9b5d3ee4473
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-01-30 23:46:11 +0300

    Fix logic of brain_gcd game
    Add new function to find a correct answer
    (elimination #2 of remarks of the mentor)

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -6,19 +6,24 @@ RANGE_MIN = 1
 RANGE_MAX = 100
 
 
+def find_gcd(num1, num2):
+    if num1 == num2:
+        return num1
+
+    check_range = max(num1, num2) // 2
+    result = 1
+    for i in range(2, check_range + 1):
+        if num1 % i == 0 and num2 % i == 0:
+            result = i
+    return result
+
+
 def get_task():
     number1 = randrange(RANGE_MIN, RANGE_MAX)
     number2 = randrange(RANGE_MIN, RANGE_MAX)
 
     task = f'{number1} {number2}'
 
-    if number1 == number2:
-        return task, str(number1)
+    correct_answer = str(find_gcd(number1, number2))
 
-    check_range = max(number1, number2) // 2
-    result = 1
-    for i in range(2, check_range + 1):
-        if number1 % i == 0 and number2 % i == 0:
-            result = i
-
-    return task, str(result)
+    return task, correct_answer
