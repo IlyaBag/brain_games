ad5203eb8df4c7c78018cfd08e880d871f82a614
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-01-30 23:18:22 +0300

    Fix logic of brain_calc game
    Add new function to find a correct answer
    (elimination #2 of remarks of the mentor)

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -6,19 +6,24 @@ RANGE_MIN = 1
 RANGE_MAX = 100
 
 
+def calculate(num1, num2, operator):
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    return result
+
+
 def get_task():
     number1 = randrange(RANGE_MIN, RANGE_MAX)
     number2 = randrange(RANGE_MIN, RANGE_MAX)
-    index = randrange(3)
     operators = ('+', '-', '*')
+    index = randrange(len(operators))
 
     task = f'{number1} {operators[index]} {number2}'
 
-    if index == 0:
-        result = number1 + number2
-    if index == 1:
-        result = number1 - number2
-    if index == 2:
-        result = number1 * number2
+    correct_answer = str(calculate(number1, number2, operators[index]))
 
-    return task, str(result)
+    return task, correct_answer
