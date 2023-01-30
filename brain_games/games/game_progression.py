4623cfc67d951325406fb0defc4a56610610bf2b
Ilya Bagrov <ilya.b.only@gmail.ru>
2023-01-31 01:13:41 +0300

    Fix logic of brain_progression game
    Add new function to find a correct answer
    (elimination #2 of remarks of the mentor)

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -4,22 +4,28 @@ from random import randrange
 GAME_RULE = 'What number is missing in the progression?'
 
 
+def hide_sequence_element(items):
+    hidden_index = randrange(1, len(items) - 2)
+    hidden_elem = items[hidden_index]
+
+    items[hidden_index] = '..'
+    hid_elem_items = ''
+    for elem in items:
+        hid_elem_items = f'{hid_elem_items} {elem}'
+
+    return hid_elem_items.lstrip(), hidden_elem
+
+
 def get_task():
     start = randrange(1, 51)
-    com_dif = randrange(1, 6)  # common difference
+    com_dif = randrange(1, 6)  # common difference of an arithmetic sequence
     seq_length = randrange(5, 15)
-    hidden_index = randrange(1, seq_length - 1)
-    sequence = [start]
 
-    for i in range(1, seq_length):
+    sequence = [start]
+    for _ in range(1, seq_length):
         start += com_dif
         sequence.append(start)
 
-    result = sequence[hidden_index]
+    task, result = hide_sequence_element(sequence)
 
-    sequence[hidden_index] = '..'
-    task = ''
-    for elem in sequence:
-        task = f'{task} {elem}'
-
-    return task.lstrip(), str(result)
+    return task, str(result)
